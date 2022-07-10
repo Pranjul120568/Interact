644a6d409ae96d1b7a6d319649ae25080bd35745
Pranjul Dwivedi <pranjuldwivedi120568@gmail.com>
2022-07-10 22:38:45 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -12,7 +12,7 @@ main()
 
 async function main() {
   return await mongoose.connect(
-    // "mongodb+srv://Pranjul120568:pranjul@cluster0.7jfm9sx.mongodb.net/?retryWrites=true&w=majority"
+    
     "mongodb://localhost:27017/myapp"
   );
 }
